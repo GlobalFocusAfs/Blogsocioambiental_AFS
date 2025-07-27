8dcaf32b4762fef8c00463f1dcd1f6ba22b05d54
globalfocus <franciscocauanoliveiro@gmail.com>
2025-07-27 16:53:28 -0300

    Sua mensagem de commit

--- socioambiental-blog/server/src/main/java/com/example/socioambiental/config/MongoConfig.java
+++ socioambiental-blog/server/src/main/java/com/example/socioambiental/config/MongoConfig.java
@@ -16,7 +16,6 @@ public class MongoConfig {
 
     @Bean
     public MongoClient mongoClient() {
-        // Removed ssl=true and tls=true from connection string to avoid redundancy
         String connectionString = "mongodb+srv://usuarioBlog:usuarioBlog@cluster0.bqekblt.mongodb.net/socioambiental-blog?retryWrites=true&w=majority&tlsAllowInvalidCertificates=true";
 
         MongoClientSettings settings = MongoClientSettings.builder()
@@ -24,24 +23,13 @@ public class MongoConfig {
                 .applyToSslSettings(builder -> {
                     builder.enabled(true);
                     builder.invalidHostNameAllowed(true); // Align with application.properties
-                    builder.context(getSSLContext());
+                    // Use default SSLContext instead of forcing TLSv1.2
                 })
                 .applyToClusterSettings(builder ->
                         builder.serverSelectionTimeout(30, TimeUnit.SECONDS))
                 .build();
 
         return MongoClients.create(settings);
-    }
-
-    private SSLContext getSSLContext() {
-        try {
-            // Explicitly set SSLContext to TLSv1.2 for compatibility
-            SSLContext sslContext = SSLContext.getInstance("TLSv1.2");
-            sslContext.init(null, null, null);
-            return sslContext;
-        } catch (Exception e) {
-            throw new RuntimeException("Failed to get TLSv1.2 SSLContext", e);
-        }
     }
 
     @Bean
