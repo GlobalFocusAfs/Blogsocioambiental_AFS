3a0b5f89e6e403b61fc1bc172d72d88600a282e7
globalfocus <franciscocauanoliveiro@gmail.com>
2025-08-07 07:15:43 -0300

    Sua mensagem 4de commipelo amor de Deust

--- socioambiental-blog/client/src/utils/keepAlive.js
+++ socioambiental-blog/client/src/utils/keepAlive.js
@@ -13,7 +13,7 @@ const KEEP_ALIVE_CONFIG = {
     backendUrl: 'https://blogsocioambiental-afs-1.onrender.com',
     
     // Intervalo entre requisições (em milissegundos)
-    interval: 10 * 60 * 1000, // 10 minutos para não sobrecarregar o Render
+    interval: 5 * 60 * 1000, // 5 minutos para manter o backend ativo
     
     // Ativar/desativar o keep-alive
     enabled: true,
