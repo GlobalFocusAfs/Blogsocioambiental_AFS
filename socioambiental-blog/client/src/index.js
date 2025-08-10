cbc11aa6b6c16c53544b119cd8f948eee0fa4a55
globalfocus <franciscocauanoliveiro@gmail.com>
2025-08-10 20:12:36 -0300

    Sua mensagem 4de commipelo novon  de Deust

--- socioambiental-blog/client/src/index.js
+++ socioambiental-blog/client/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles.css';
 import './styles-professional.css';
+import './styles-mobile-fix.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
