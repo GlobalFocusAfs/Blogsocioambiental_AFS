4d9b07bfee7fe1b8533bdd8ee664c9a7dae16f44
globalfocus <franciscocauanoliveiro@gmail.com>
2025-08-10 19:06:34 -0300

    Sua mensagem 4de commipelo novon  de Deust

--- socioambiental-blog/client/src/index.js
+++ socioambiental-blog/client/src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './styles.css';
 import './styles-professional.css';
@@ -8,8 +7,6 @@ import './styles-professional.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
